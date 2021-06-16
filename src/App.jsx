b012d05053c667a1dd29930507b5dac2e316ee1d
Blaxion <khibaziev.f@gmail.com>
2021-06-16 12:22:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
-export default class App extends React.Component{
-  
-  state={
-    myTab:['a','b','c','d','e'],
-    myTabL:5
+export default class App extends React.Component {
+  tab = ['a', 'b', 'c', 'd', 'e'];
+  state = {
+    p1: this.tab[0],
+    p2: this.tab[1],
+    p3: this.tab[2],
+    p4: this.tab[3],
+    p5: this.tab[4]
   }
-  
-  removeEl = () =>{
-    
-    this.setState = ({
-      myTab  : this.state.myTab.pop(),
-      myTabL : this.state.myTab.length
-    })
-    console.log(this.state.myTab.length)
-    console.log(this.state.myTab)
-    
+  stateL = Object.keys(this.state).length;
+  removeEl = () => {
+    delete this.state[Object.keys(this.state)[0]];
+    this.stateL = Object.keys(this.state).length;
+    this.backgrounding(this.stateL)
+    this.setState({});
   }
-  
+  backgrounding = (len) =>{
+    if (len === 1 ) {
+      document.body.style = "background-color : orange"
+    }else if (len === 0) {
+      document.body.style = "background-color : red"
+    }
+  }
   render() {
-    return(
+    return (
       <div>
-        <p>{this.state.myTab}</p>
+        <p>{this.stateL}</p>
         <button onClick={this.removeEl}>Remove Element</button>
       </div>
     );
